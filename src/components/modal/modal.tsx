9b17c1c1efcef7b6fbad37c74e21682af0588a9d
DanyPlaya <danila.lusuy@gmail.com>
2023-12-14 21:00:20 +0300

    fix: remove memo

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -14,22 +14,35 @@ interface ModalProps {
   filter: string | null;
 }
 
-export const Modal: FC<ModalProps> = memo(
-  ({ options, handleModal, type, filter }) => {
-    return (
-      <div className={styles.modal}>
-        <span className={styles.modal__close}>
-          <CloseModal width="20px" height="20px" onClick={handleModal} />
-        </span>
-        {options && Object.keys(options).length > 0 ? (
-          <div className={styles.modal__body}>
-            <SearchInput type={type} />
-            <div className={styles.modal__filter}>
-              <OptionFilter categories={options} />
-            </div>
-            <div className={styles.modal__options}>
-              {filter
-                ? options[filter]?.map((option) => (
+export const Modal: FC<ModalProps> = ({
+  options,
+  handleModal,
+  type,
+  filter,
+}) => {
+  return (
+    <div className={styles.modal}>
+      <span className={styles.modal__close}>
+        <CloseModal width="20px" height="20px" onClick={handleModal} />
+      </span>
+      {options && Object.keys(options).length > 0 ? (
+        <div className={styles.modal__body}>
+          <SearchInput type={type} />
+          <div className={styles.modal__filter}>
+            <OptionFilter categories={options} />
+          </div>
+          <div className={styles.modal__options}>
+            {filter
+              ? options[filter]?.map((option) => (
+                  <Option
+                    key={option.code_name}
+                    option={option}
+                    handleModal={handleModal}
+                    type={type}
+                  />
+                ))
+              : Object.keys(options).map((category) =>
+                  options[category]?.map((option) => (
                     <Option
                       key={option.code_name}
                       option={option}
@@ -37,25 +50,15 @@ export const Modal: FC<ModalProps> = memo(
                       type={type}
                     />
                   ))
-                : Object.keys(options).map((category) =>
-                    options[category]?.map((option) => (
-                      <Option
-                        key={option.code_name}
-                        option={option}
-                        handleModal={handleModal}
-                        type={type}
-                      />
-                    ))
-                  )}
-            </div>
+                )}
           </div>
-        ) : (
-          <div className={styles.modal__empty}>
-            <div style={{ fontSize: "74px", marginBottom: "20px" }}>:{"("}</div>
-            <div>Список пуст...</div>
-          </div>
-        )}
-      </div>
-    );
-  }
-);
+        </div>
+      ) : (
+        <div className={styles.modal__empty}>
+          <div style={{ fontSize: "74px", marginBottom: "20px" }}>:{"("}</div>
+          <div>Список пуст...</div>
+        </div>
+      )}
+    </div>
+  );
+};
