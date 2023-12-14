2b7b57e8f4fb412abe81630f89cb493dc21593c2
DanyPlaya <danila.lusuy@gmail.com>
2023-12-14 17:51:01 +0300

    feat: created queryKey for chached

--- src/assets/consts.ts
+++ src/assets/consts.ts
@@ -1,3 +1,11 @@
+
+// App routes
 export const MAIN_ROUTE = "/";
 export const ABOUT_ROUTE = "/about";
 export const SERVICES_ROUTE = "/services";
+
+
+// Query chache key
+export const optionsKey = 'Options'
+export const availableKey = 'Available'
+export const exchangersKey = 'Exchangers'
