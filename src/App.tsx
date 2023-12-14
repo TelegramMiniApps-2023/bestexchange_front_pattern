eec8e2cc0249d04a1ac960c01d342deb013ee0ea
DanyPlaya <danila.lusuy@gmail.com>
2023-12-14 17:49:58 +0300

    feat: wrapped QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { FC, ReactElement } from "react";
 import { AppRouter } from "./pages/appRouter";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/header";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./api/queryClient";
 
 const App: FC = (): ReactElement => {
   return (
     <div className="main__wrapper">
-      <BrowserRouter>
-        <Header />
-        <AppRouter />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Header />
+          <AppRouter />
+        </BrowserRouter>
+      </QueryClientProvider>
     </div>
   );
 };
