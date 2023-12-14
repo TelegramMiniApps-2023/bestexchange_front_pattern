476f289572ee9a9c55ce218f3ad64d9439f5038a
DanyPlaya <danila.lusuy@gmail.com>
2023-12-14 17:52:44 +0300

    fix: refactoring

--- src/components/optionFilter/index.tsx
+++ src/components/optionFilter/index.tsx
@@ -1,45 +1,41 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
+import { Categories } from "../../model/Categories";
 import styles from "./styles.module.scss";
-import { ICategories } from "../../model/ICategories";
-import { Context } from "../../main";
-import { observer } from "mobx-react-lite";
 
 interface OptionFilterProps {
-  categories: ICategories;
+  categories: Categories;
 }
 
-export const OptionFilter: FC<OptionFilterProps> = observer(
-  ({ categories }) => {
-    const { store } = useContext(Context);
-    const handleCategory = (category: string | null) => {
-      store.setFilter(category);
-    };
-    return (
-      <div className={styles.filter}>
-        <div
-          className={
-            !store.filter
-              ? `${styles.filter__item} ${styles.active}`
-              : styles.filter__item
-          }
-          onClick={() => handleCategory(null)}
-        >
-          Все
-        </div>
-        {Object.keys(categories).map((category, index) => (
-          <div
-            onClick={() => handleCategory(category)}
-            key={index}
-            className={
-              category === store.filter
-                ? `${styles.filter__item} ${styles.active}`
-                : styles.filter__item
-            }
-          >
-            {category}
-          </div>
-        ))}
+export const OptionFilter: FC<OptionFilterProps> = ({ categories }) => {
+  // const { store } = useContext(Context);
+  const handleCategory = (category: string | null) => {
+    // store.setFilter(category);
+  };
+  return (
+    <div className={styles.filter}>
+      <div
+        // className={
+        //   !store.filter
+        //     ? `${styles.filter__item} ${styles.active}`
+        //     : styles.filter__item
+        // }
+        onClick={() => handleCategory(null)}
+      >
+        Все
       </div>
-    );
-  }
-);
+      {Object.keys(categories).map((category, index) => (
+        <div
+          onClick={() => handleCategory(category)}
+          key={index}
+          // className={
+          //   category === store.filter
+          //     ? `${styles.filter__item} ${styles.active}`
+          //     : styles.filter__item
+          // }
+        >
+          {category}
+        </div>
+      ))}
+    </div>
+  );
+};
