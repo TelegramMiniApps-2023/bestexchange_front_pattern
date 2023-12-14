7f5c59fb0f8b299e9ee58e4366ee22feb7fbfd9a
DanyPlaya <danila.lusuy@gmail.com>
2023-12-14 23:22:03 +0300

    feat: rewritten in react-query

--- src/components/option/option.tsx
+++ src/components/option/option.tsx
@@ -5,6 +5,7 @@ import styles from "./styles.module.scss";
 import { useFetchAvailable } from "../../api/api";
 import { Options } from "../../model/Options";
 import { queryClient } from "../../api/queryClient";
+import { availableKey, exchangersKey } from "../../assets/consts";
 
 interface OptionProps {
   option: Options;
@@ -22,15 +23,16 @@ export const Option: FC<OptionProps> = ({ option, handleModal, type }) => {
     if (type === "give") {
       setGiveSelect(option);
       setGetSelect(null);
-      refetch();
+      queryClient.removeQueries(exchangersKey);
+      // refetch();
     } else {
       setGetSelect(option);
     }
     handleModal();
   };
-  const { refetch } = useFetchAvailable({
-    base: giveSelect?.code_name,
-  });
+  // const { refetch } = useFetchAvailable({
+  //   base: giveSelect?.code_name,
+  // });
   return (
     <div className={styles.option} onClick={() => handleChangeDirection()}>
       <div className={styles.option__img}>
