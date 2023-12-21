3114ca4d0cd790caec07fa36e91cff3fe6978315
DanyPlaya <danila.lusuy@gmail.com>
2023-12-21 13:44:50 +0300

    feat: added new cache key for react query

--- src/assets/consts.ts
+++ src/assets/consts.ts
@@ -5,7 +5,12 @@ export const ABOUT_ROUTE = "/about";
 export const SERVICES_ROUTE = "/services";
 
 
-// Query chache key
+// Query chache key no cash
 export const optionsKey = 'Options'
 export const availableKey = 'Available'
 export const exchangersKey = 'Exchangers'
+// Query chache key cash
+export const countriesKey = 'Countries'
+export const citiesKey = 'Cities'
+export const availableValutesKey = 'AvailableValutes'
+export const directionCashKey = 'directionCashKey'
