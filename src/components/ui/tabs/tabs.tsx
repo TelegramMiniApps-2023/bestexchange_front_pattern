4b5b82eb9d3a03000e67dcdf3ec2792f0bd25690
DanyPlaya <danila.lusuy@gmail.com>
2023-12-27 21:36:12 +0300

    feat: added classNameTab,classNameTabItem props

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -5,24 +5,25 @@ import clsx from "clsx";
 import styles from "./tabs.module.scss";
 import { memo } from "react";
 export type TabsItem = {
-  value: string;
+  value: string | null;
   content: string;
 };
 type TabsProps = {
   onTabClick: (tab: TabsItem) => void;
   tabs: TabsItem[];
-  className?: string;
+  classNameTab?: string;
+  classNameTabItem?: string;
   filter?: string | null;
 };
 export const Tabs = memo((props: TabsProps) => {
-  const { onTabClick, tabs, filter, className } = props;
+  const { onTabClick, tabs, filter, classNameTab, classNameTabItem } = props;
 
   return (
-    <div className={clsx(styles.tabs)}>
+    <div className={clsx(styles.tabs, classNameTab)}>
       {tabs.map((tab) => (
         <div
-          className={clsx(styles.tabs__item, className, {
-            [styles.active]: filter === tab.value,
+          className={clsx(styles.tabs__item, classNameTabItem, {
+            [styles.active]: filter === tab.value || null,
           })}
           onClick={() => onTabClick(tab)}
           key={tab.content}
