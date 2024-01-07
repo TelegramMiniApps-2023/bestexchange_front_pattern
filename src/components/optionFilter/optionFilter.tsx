108e14b650d512233cedd2597ed591c44c8e699e
Senez <113931250+DanyPlaya@users.noreply.github.com>
2024-01-08 01:56:09 +0300

    feat: added tab component

--- src/components/optionFilter/optionFilter.tsx
+++ src/components/optionFilter/optionFilter.tsx
@@ -6,80 +6,87 @@ import { Tabs } from "../ui/tabs";
 import { TabsItem } from "../ui/tabs/tabs";
 import styles from "./styles.module.scss";
 import { IconLeft } from "../../assets/icons/IconLeft";
-
+import { Carousel, ScrollingCarousel } from '@trendyol-js/react-carousel';
+import { Tab } from "../ui/tabs/tabsItem";
 interface OptionFilterProps {
-  categories: Categories;
+    categories: Categories;
 }
 
 export const OptionFilter: FC<OptionFilterProps> = memo(({ categories }) => {
-  const filter = useFiltersStore((state) => state.filter);
-  const setFilter = useFiltersStore((state) => state.setFilter);
-  const search = useFiltersStore((state) => state.search);
-  // const [activeTabIndex, setActiveTabIndex] = useState(0);
-  const [offsetX, setOffsetX] = useState(0);
+    const filter = useFiltersStore((state) => state.filter);
+    const setFilter = useFiltersStore((state) => state.setFilter);
+    const search = useFiltersStore((state) => state.search);
+    // const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [offsetX, setOffsetX] = useState(0);
 
-  const filteredCategories = Object.keys(categories).filter((category) =>
-    categories[category]?.some((option) =>
-      option.name.toLowerCase().includes(search.toLowerCase())
-    )
-  );
+    const filteredCategories = Object.keys(categories).filter((category) =>
+        categories[category]?.some((option) =>
+            option.name.toLowerCase().includes(search.toLowerCase())
+        )
+    );
 
-  const tabsItem: TabsItem[] = useMemo(
-    () => [
-      { value: null, content: "Все" },
-      ...filteredCategories.map((category) => ({
-        value: category,
-        content: category,
-      })),
-    ],
-    [filteredCategories]
-  );
+    const tabsItem: TabsItem[] = useMemo(
+        () => [
+            { value: null, content: "Все" },
+            ...filteredCategories.map((category) => ({
+                value: category,
+                content: category,
+            })),
+        ],
+        [filteredCategories]
+    );
 
-  const handleCategory = (category: string | null) => {
-    setFilter(category);
-    if (tabsItem.length < 3) return;
-    if (category === null) {
-      setOffsetX(0);
-      return;
-    }
+    const handleCategory = (category: string | null) => {
+        setFilter(category);
+        //if (tabsItem.length < 3) return;
+        //if (category === null) {
+        //    setOffsetX(0);
+        //    return;
+        //}
 
-    const categoryIndex = tabsItem.findIndex((tab) => tab.value === category);
-    setOffsetX(-100 * categoryIndex);
-  };
-  const handleNextTab = () => {
-    if (tabsItem.length < 3) return;
-    setOffsetX((currentOffset) => {
-      const newOffset = currentOffset - 100;
-      const maxOffset = -(100 * tabsItem.length - 1);
-      return Math.max(newOffset, maxOffset);
-    });
-  };
-  const handlePrevTab = () => {
-    setOffsetX((currentOffset) => {
-      const newOffset = currentOffset + 100;
+        //const categoryIndex = tabsItem.findIndex((tab) => tab.value === category);
+        //setOffsetX(-100 * categoryIndex);
+    };
+    //const handleNextTab = () => {
+    //    if (tabsItem.length < 3) return;
+    //    setOffsetX((currentOffset) => {
+    //        const newOffset = currentOffset - 100;
+    //        const maxOffset = -(100 * tabsItem.length - 1);
+    //        return Math.max(newOffset, maxOffset);
+    //    });
+    //};
+    //const handlePrevTab = () => {
+    //    setOffsetX((currentOffset) => {
+    //        const newOffset = currentOffset + 100;
 
-      return Math.min(newOffset, 0);
-    });
-  };
+    //        return Math.min(newOffset, 0);
+    //    });
+    //};
 
-  useEffect(() => {
-    setFilter(null);
-    setOffsetX(0);
-  }, [search, setFilter]);
+    //useEffect(() => {
+    //    setFilter(null);
+    //    setOffsetX(0);
+    //}, [search, setFilter]);
 
-  return (
-    <div className={styles.filterWrapper}>
-      <IconLeft className={styles.nextTab} onClick={handlePrevTab} />
-      <Tabs
-        onTabClick={(tab) => handleCategory(tab.value)}
-        tabs={tabsItem}
-        classNameTab={styles.filter}
-        filter={filter}
-        classNameTabItem={styles.filter_tab_item}
-        // activeTabIndex={activeTabIndex}
-        offsetX={offsetX}
-      />
-      <IconRight className={styles.nextTab} onClick={handleNextTab} />
-    </div>
-  );
+    return (
+        //<div className={styles.filterWrapper}>
+        //{/*<IconLeft className={styles.nextTab} onClick={handlePrevTab} />*/}
+
+        //{/*<Tabs
+        //        onTabClick={(tab) => handleCategory(tab.value)}
+        //        tabs={tabsItem}
+        //        classNameTab={styles.filter}
+        //        filter={filter}
+        //        classNameTabItem={styles.filter_tab_item}
+        //        // activeTabIndex={activeTabIndex}
+        //        offsetX={offsetX}
+        //    />*/}
+        <Carousel dynamic infinite={false} show={2.5} slide={1} swiping className={styles.filter} leftArrow={<IconLeft />} rightArrow={<IconRight />}>
+            {tabsItem.map(tab => <Tab onTabClick={(tab) => handleCategory(tab.value)} classNameTabItem={styles.filter_tab_item} tab={tab} filter={filter} key={tab.content} />)}
+        </Carousel>
+
+
+        //    {/*<IconRight className={styles.nextTab} onClick={handleNextTab} />*/}
+        //</div>
+    );
 });
--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -3,47 +3,39 @@
 import clsx from "clsx";
 import { memo } from "react";
 import styles from "./tabs.module.scss";
+import { Tab } from "./tabsItem";
 
 export type TabsItem = {
-  value: string | null;
-  content: string;
+    value: string | null;
+    content: string;
 };
 
 type TabsProps = {
-  onTabClick: (tab: TabsItem) => void;
-  tabs: TabsItem[];
-  classNameTab?: string;
-  classNameTabItem?: string;
-  filter?: string | null;
-  activeTabIndex?: number;
-  offsetX?: number;
+    onTabClick: (tab: TabsItem) => void;
+    tabs: TabsItem[];
+    classNameTab?: string;
+    classNameTabItem?: string;
+    filter?: string | null;
+    activeTabIndex?: number;
+    offsetX?: number;
 };
 
 export const Tabs = memo((props: TabsProps) => {
-  const {
-    onTabClick,
-    tabs,
-    filter,
-    classNameTab,
-    classNameTabItem,
-    activeTabIndex = 0,
-    offsetX = 0,
-  } = props;
+    const {
+        onTabClick,
+        tabs,
+        filter,
+        classNameTab,
+        classNameTabItem,
+        activeTabIndex = 0,
+        offsetX = 0,
+    } = props;
 
-  return (
-    <div className={clsx(styles.tabs, classNameTab)}>
-      {tabs.map((tab) => (
-        <div
-          style={{ transform: `translateX(${offsetX}px)` }}
-          className={clsx(styles.tabs__item, classNameTabItem, {
-            [styles.active]: filter === tab.value,
-          })}
-          onClick={() => onTabClick(tab)}
-          key={tab.content}
-        >
-          {tab.content}
+    return (
+        <div className={clsx(styles.tabs, classNameTab)}>
+            {tabs.map((tab) => (
+                <Tab classNameTabItem={classNameTabItem} onTabClick={onTabClick} tab={tab} filter={filter} key={tab.value} />
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 });
