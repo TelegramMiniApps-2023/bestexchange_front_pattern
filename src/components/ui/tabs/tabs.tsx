19d6132df64f9dfbe99b5bcc6e0c35624048ec3c
AbsSh <macbro@Abdsh.local>
2024-01-11 18:06:44 +0500

    feat: merge resultArrow branch

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -1,9 +1,7 @@
-// tabs.tsx
-
 import clsx from "clsx";
 import { memo } from "react";
 import styles from "./tabs.module.scss";
-
+import { Tab } from "./tab";
 export type TabsItem = {
   value: string | null;
   content: string;
@@ -20,29 +18,18 @@ type TabsProps = {
 };
 
 export const Tabs = memo((props: TabsProps) => {
-  const {
-    onTabClick,
-    tabs,
-    filter,
-    classNameTab,
-    classNameTabItem,
-    activeTabIndex = 0,
-    offsetX = 0,
-  } = props;
+  const { onTabClick, tabs, filter, classNameTab, classNameTabItem } = props;
 
   return (
     <div className={clsx(styles.tabs, classNameTab)}>
       {tabs.map((tab) => (
-        <div
-          style={{ transform: `translateX(${offsetX}px)` }}
-          className={clsx(styles.tabs__item, classNameTabItem, {
-            [styles.active]: filter === tab.value,
-          })}
-          onClick={() => onTabClick(tab)}
-          key={tab.content}
-        >
-          {tab.content}
-        </div>
+        <Tab
+          classNameTabItem={classNameTabItem}
+          onTabClick={onTabClick}
+          tab={tab}
+          filter={filter}
+          key={tab.value}
+        />
       ))}
     </div>
   );
