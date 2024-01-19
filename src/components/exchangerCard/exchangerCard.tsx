9ce03e38147f70668b3a67c33954c1684cc57c89
AbsSh <macbro@Abdsh.local>
2024-01-19 15:45:03 +0500

    fix: countries scroll, modal relative, link in exchangerCard etc.

--- src/components/exchangerCard/exchangerCard.tsx
+++ src/components/exchangerCard/exchangerCard.tsx
@@ -11,32 +11,32 @@ interface ExchangerCardProps {
 export const ExchangerCard: FC<ExchangerCardProps> = ({ card }) => {
   const { t } = useTranslation();
   return (
-    // <a href={card.partner_link} target="_blank" rel="noopener noreferrer">
-    // </a>
     <article className={styles.exchangerItem}>
-      <header>
-        <hgroup>
-          <h2>{card.name}</h2>
-          <h3>{t("Онлайн обмен")}</h3>
-        </hgroup>
-      </header>
-      <hr />
-      <footer>
-        <hgroup>
-          <h2>
-            {card.in_count} <span>{card.valute_from}</span>
-            <i>
-              <ArrowRight />
-            </i>
-          </h2>
-          <h2>
-            {card.out_count} <span>{card.valute_to}</span>
-          </h2>
-        </hgroup>
-        <span>
-          {t("Обмен")} {card.min_amount} {t("до")} {card.max_amount}
-        </span>
-      </footer>
+      <a href={card.partner_link} target="_blank" rel="noopener noreferrer">
+        <header>
+          <hgroup>
+            <h2>{card.name}</h2>
+            <h3>{t("Онлайн обмен")}</h3>
+          </hgroup>
+        </header>
+        <hr />
+        <footer>
+          <hgroup>
+            <h2>
+              {card.in_count} <span>{card.valute_from}</span>
+              <i>
+                <ArrowRight />
+              </i>
+            </h2>
+            <h2>
+              {card.out_count} <span>{card.valute_to}</span>
+            </h2>
+          </hgroup>
+          <span>
+            {t("Обмен")} {card.min_amount} {t("до")} {card.max_amount}
+          </span>
+        </footer>
+      </a>
     </article>
   );
 };
