3e69bb9b5872bf9dd894243dc01afacc42d1fe6a
AbsSh <macbro@Abdsh.local>
2024-01-24 15:53:23 +0500

    fix: userData

--- src/telegram.d.ts
+++ src/telegram.d.ts
@@ -11,7 +11,18 @@ declare global {
           };
           initData: string;
           initDataUnsafe: {
-            id: number
+            user: {
+              id: number;
+              is_bot: boolean;
+              first_name: string;
+              last_name: string;
+              username: string;
+              language_code: string;
+              is_premium: boolean;
+              added_to_attachment_menu: boolean;
+              allows_write_to_pm: boolean;
+              photo_url: string
+            }
           }
         };
       };
