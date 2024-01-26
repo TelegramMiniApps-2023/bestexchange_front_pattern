ed2e5ec75c48c71f446d769eba5b051cd66dad97
AbsSh <macbro@Abdsh.local>
2024-01-26 16:27:01 +0500

    fix: preloader time 1000ms

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -27,7 +27,7 @@ export const MainPage = () => {
       clearInterval(interval);
       setPreloaderFinished((prev) => !prev);
       tg.expand();
-    }, 1500);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, []);
@@ -43,7 +43,7 @@ export const MainPage = () => {
             [styles.preloaderFullHeight]: tg.isExpanded,
           })}
         >
-          <Preloader step={20} progress={progress} strokeWidth={20} />
+          <Preloader step={28} progress={progress} strokeWidth={20} />
         </div>
       )}
     </div>
