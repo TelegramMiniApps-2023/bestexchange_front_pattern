48505d7c818db40af480ada8163a7e5044e2f552
AbsSh <macbro@Abdsh.local>
2024-02-12 21:25:24 +0500

    fix: telegram methods types

--- src/telegram.d.ts
+++ src/telegram.d.ts
@@ -6,22 +6,22 @@ declare global {
           enableClosingConfirmation: () => void;
           expand: () => void;
           ready: () => void;
-          WebAppUser?: {
+          WebAppUser: {
             id: number;
           };
           initData: string;
           initDataUnsafe: {
             user: {
-              user_id?: number;
-              is_bot?: boolean;
-              first_name?: string;
-              last_name?: string;
-              username?: string;
-              language_code?: string;
-              is_premium?: boolean;
-              added_to_attachment_menu?: boolean;
-              allows_write_to_pm?: boolean;
-              photo_url?: string
+              user_id: number;
+              is_bot: boolean;
+              first_name: string;
+              last_name: string;
+              username: string;
+              language_code: string;
+              is_premium: boolean;
+              added_to_attachment_menu: boolean;
+              allows_write_to_pm: boolean;
+              photo_url: string
             }
           };
           openLink: (url: string, options?: {try_instant_view: boolean}[]) => void;
